111f35d0eabbf6374f7b1a2460c6bf880e0256f5
victormpcmun@gmail.com <victormpcmun@gmail.com>
2021-05-26 19:49:45 +0200

    test

--- src/main/java/com/github/victormpcmun/dngsettingsinterpolator/service/SettingRangeService.java
+++ src/main/java/com/github/victormpcmun/dngsettingsinterpolator/service/SettingRangeService.java
@@ -29,7 +29,7 @@ public class SettingRangeService {
                 settingRangeList.add(new SettingRange(setting, initValue, endValue, DECIMALS));
             } else {
                 messageService.message(
-                        "Skipping setting " + setting + " because its value is not defined in " + initFile + " or " + endFile + " or both are the same");
+                        "Skipping setting " + setting + " because its value is not defined in " + initFile + " or " + endFile ;
             }
 
 
@@ -40,7 +40,7 @@ public class SettingRangeService {
 
 
     private boolean hasToCalculateSetting(String initValue, String endValue)  {
-        return initValue==null || endValue==null || initValue.equals(endValue);
+        return initValue==null || endValue==null;
     }
 
 }
